a47302af36ed3d304de5523d0a948981ef7c5147
Sajal Sharma <sajal2692@gmail.com>
2018-01-12 21:19:23 +0530

    feat: initial commit; added tab navigator

---
+++ App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Text, View, Platform } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation'
+import ListDecks from './components/ListDecks'
+import AddDeck from './components/AddDeck'
+import { MaterialCommunityIcons, FontAwesome, Ionicons } from '@expo/vector-icons'
+import { white, black, purple } from './utils/colors'
+
+const Tabs = TabNavigator({
+  ListDecks: {
+    screen: ListDecks,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='cards' size={27} color={tintColor} />
+    },
+  },
+  AddDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'Add Deck',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={27} color={tintColor} />
+    },
+  },
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: white,
+    style: {
+      height: 50,
+      backgroundColor: black,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Tabs />
+    );
+  }
+}
---
+++ components/ListDecks.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+class ListDecks extends Component {
+  render() {
+    return (
+      <View>
+        <Text>List Decks</Text>
+      </View>
+    );
+  }
+}
+
+export default ListDecks
